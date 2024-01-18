7c8b979654b41bbbf735d4021b93c7315b299048
Josh Bostick <jbostick99@gmail.com>
2024-01-18 15:33:13 -0600

    finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (word == null || word.Length == 0)
+            {
+                return false;
+            }
+            string lowered = word.ToLower();
+            string reversed = "";
+
+            for (int i = lowered.Length - 1; i >= 0; i--)
+            {
+                reversed += lowered[i];
+            }
+            if (lowered == reversed)
+            {
+                return true;
+            }
+            return false;
+
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        
+        [Theory]
+        [InlineData("madam", true)]
+        [InlineData("racecar", true)]
+        [InlineData("mother", false)]
+        [InlineData("butterfly", false)]
+        [InlineData("Level", true)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsAPalindrome(string word, bool expected)
         {
+            var x = new WordSmith();
+            var actual = x.IsAPalindrome(word);
+            Assert.Equal(actual, expected);
 
         }
     }
